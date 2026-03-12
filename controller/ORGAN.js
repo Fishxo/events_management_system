8a9e823e9442b71e1de94016484e0c5d5b228dd1
Fisiha <fishbrnd@gmail.com>
2026-03-12 14:55:37 +0300

    adding the organizer page from controller

--- controller/ORGAN.js
+++ controller/ORGAN.js
@@ -1,4 +1,21 @@
+const event = require("../models/event");
 
 exports.CREATEevent = (req,res) => {
     res.render('CREATEeventsForOrgan')
+}
+
+//getting the delete requiest from organizer and deleting an event
+exports.delete = async(req,res) => {
+    const {eventId} = req.body;
+     try{
+        //getting session
+        const organizerId = req.session.attenderId;
+
+        const eve = await event.find({organId : organizerId})
+        await event.findByIdAndDelete(eventId)
+        res.render('MINEeventsFOROrgan',{eve})
+     }catch(err){
+        console.log(err)
+        res.send('sorry could not delete it')
+     }
 }
